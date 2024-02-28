09e78f23747e8437f50c52b6de2c83967f136f5e
arpitnuwal <arpitnuwal1996@gmail.com>
2024-02-28 10:34:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./css/app.css";
+import css from "./css/app.module.css";
+import { Head } from "./css/css";
+
+function App() {
+  // const a = { color: "blue", fontSize: "40px" }
+  return (
+    <>
+      <Head>hello</Head>
+    </>
+  );
+}
+
+export default App;
---
+++ src/css/css.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Head = styled.div`
+  font-size: 40px;
+  color: red;
+`;
+
+export { Head };
